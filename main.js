70a4611b07ce239e01fe5fbb4aaedb4e32eb4f01
pei166 <konpei16658@gmail.com>
2020-05-04 23:38:21 +0900

    change the mino's color

--- main.js
+++ main.js
@@ -14,8 +14,9 @@ let minoY = 0;
 let mino;
 let gameOver = false;
 let lineCount = 0;
+let minoType;
 
-var field = [ 
+let field = [ 
   [-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1], 
   [-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1], 
   [-1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1], 
@@ -50,56 +51,66 @@ let minos = [
   [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
-    [1, 1, 1, 1],
+    [2, 2, 2, 2],
     [0, 0, 0, 0],
   ],
   [
     [0, 0, 0, 0],
-    [0, 1, 1, 0],
-    [0, 1, 1, 0],
+    [0, 3, 3, 0],
+    [0, 3, 3, 0],
     [0, 0, 0, 0],
   ],
   [
     [0, 0, 0, 0],
-    [0, 1, 1, 0],
-    [1, 1, 0, 0],
+    [0, 4, 4, 0],
+    [4, 4, 0, 0],
     [0, 0, 0, 0],
   ],
   [
     [0, 0, 0, 0],
-    [1, 1, 0, 0],
-    [0, 1, 1, 0],
+    [5, 5, 0, 0],
+    [0, 5, 5, 0],
     [0, 0, 0, 0],
   ],
   [
     [0, 0, 0, 0],
-    [0, 0, 1, 0],
-    [1, 1, 1, 0],
+    [0, 0, 6, 0],
+    [6, 6, 6, 0],
     [0, 0, 0, 0],
   ],
   [
     [0, 0, 0, 0],
-    [1, 0, 0, 0],
-    [1, 1, 1, 0],
+    [7, 0, 0, 0],
+    [7, 7, 7, 0],
     [0, 0, 0, 0],
   ],
+];
+
+let minoColors = [
+  "#EE82EE",
+  "#87CEFA",
+  "#FFD700",
+  "#90EE90",
+  "#FA8072",
+  "#FFA500",
+  "#4169E1",
 ];
 
 function drawField() {
   context.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
   for (let y = 1; y < FIELD_HEIGHT; y ++) {
     for (let x = 0; x < FIELD_WIDTH; x ++) {
-      context.fillStyle = "#000000";
+      context.fillStyle = "#F5F5DC";
       context.strokeStyle = "#F5F5F5";
       if (field[y][x] === -1) context.fillStyle = "#797979";
-      if (field[y][x] === 1) context.fillStyle = "#FF0000";
+      if (field[y][x]) context.fillStyle = minoColors[field[y][x] - 1];
       context.fillRect(ORIGIN + x * BLOCK_SIZE, ORIGIN + y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
       context.strokeRect(ORIGIN + x * BLOCK_SIZE, ORIGIN + y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
     }
   }
   if (gameOver) {
     context.font = "60px serif";
-    context.fillStyle = "#FFFF00";
+    context.fillStyle = "#191970";
     context.fillText("GAME OVER", 60, 400);
   }
 }
@@ -107,9 +118,9 @@ function drawField() {
 function drawMino(mino) {
   for (let y = 0; y < MINO_SIZE; y ++ ) {
     for (let x = 0; x < MINO_SIZE; x ++) {
-      context.fillStyle = "#FF0000";
+      context.fillStyle = minoColors[minoType];
       context.strokeStyle = "#F5F5F5";
-      if (mino[y][x] == 1) {
+      if (mino[y][x]) {
         let px = ORIGIN + (4 * BLOCK_SIZE) + (x + minoX) * BLOCK_SIZE;
         let py = ORIGIN + (y + minoY) * BLOCK_SIZE;
         context.fillRect(px, py, BLOCK_SIZE, BLOCK_SIZE); 
@@ -120,7 +131,7 @@ function drawMino(mino) {
 }
 
 function drawScore() {
-  context.fillStyle = "#F5F5F5";
+  context.fillStyle = "#000000";
   context.font = "24px serif";
   context.fillText("SCORE: " + lineCount, 500, 600);
   context.fillText("Left: A", 500, 200);
@@ -135,7 +146,7 @@ function canMove(mx, my, mino) {
     for (let x = 0; x < MINO_SIZE; x ++) {
       let nx = mx + minoX + x + 4; 
       let ny = my + minoY + y;
-      if ((field[ny][nx] === -1 || field[ny][nx] === 1) && (mino[y][x] === 1)) {
+      if ((field[ny][nx] === -1 || field[ny][nx]) && (mino[y][x])) {
         return false;
       }
     }
@@ -176,7 +187,7 @@ function canTurn(mino, isLeft) {
     for (let x = 0; x < MINO_SIZE; x ++) {
       let nx = minoX + x + 4; 
       let ny = minoY + y;
-      if ((field[ny][nx] === -1 || field[ny][nx] === 1) && (turned[y][x] === 1)) {
+      if ((field[ny][nx] === -1 || field[ny][nx]) && (turned[y][x])) {
         return false;
       }
     }
@@ -187,15 +198,15 @@ function canTurn(mino, isLeft) {
 function fixMino() {
   for (let y = 0; y < MINO_SIZE; y ++) {
     for (let x = 0; x < MINO_SIZE; x ++) {
-      if (mino[y][x] == 1) {
-        field[y + minoY][x + minoX + 4] = 1;
+      if (mino[y][x]) {
+        field[y + minoY][x + minoX + 4] = minoType + 1;
       }
     }
   }
 }
 
 function makeMino() {
-  const minoType = Math.floor(Math.random() * 7);
+  minoType = Math.floor(Math.random() * 7);
   mino = minos[minoType];
   minoX = 0;
   minoY = 0;
